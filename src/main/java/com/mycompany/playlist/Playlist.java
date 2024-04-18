95e8f9cbb338a2a5ff313f7cec5079116d9905d6
jafet mtz <jafet mtz@192.168.1.70>
2024-04-17 22:31:11 -0600

    clases playlist

---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author jafet mtz
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas pl=new Musicas("Elvira","Distorsion","2024",3);
+        pl.ShowPlaylist();
+    }
+}
